ca1c89a4ca14e9a24072b93a3d275bf2e0704798
zwiastunsw <zwiastunsw@users.noreply.github.com>
2020-12-28 16:48:51 +0000

    deploy: 8934c5ef34a90c366ae17363a47822a6ecb5eaf1

--- tota11y.js
+++ tota11y.js
@@ -9,7 +9,7 @@
  * Released under the MIT license
  * http://github.com/Khan/tota11y/blob/master/LICENSE.txt
  * 
- * Date: 2020-12-27
+ * Date: 2020-12-28
  * 
  */
 /******/ (function(modules) { // webpackBootstrap
