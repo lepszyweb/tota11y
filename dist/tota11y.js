7a4f07d87a65cbb0b9084ca33ea680969b8cec0a
wojsmol <wojsmol@users.noreply.github.com>
2023-05-03 02:08:28 +0000

    deploy: 9b6dcdd7f0a84c1e11310612ed44f0ded3c7c420

--- dist/tota11y.js
+++ dist/tota11y.js
@@ -9,7 +9,7 @@
  * Released under the MIT license
  * http://github.com/Khan/tota11y/blob/master/LICENSE.txt
  *
- * Date: 2023-05-02
+ * Date: 2023-05-03
  *
  */
 /******/ (() => { // webpackBootstrap
